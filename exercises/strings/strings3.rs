3c068e2b6f45de1346522f35dafccc2ad3c18eb7
Marc Casaldaliga <marcos.casaldaliga@hp.com>
2023-02-05 10:48:48 +0100

    solved until strings3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,33 +3,39 @@
 
 // I AM NOT DONE
 
-fn trim_me(input: &str) -> String {
+pub fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
     // first and last positions of non whitespace characters in input string
-    let first:usize = 0;
-    let last:usize = 0;
+    let mut first:usize = 0;
+    let mut last:usize = 0;
     // boolean var to know if we are past the white space beggining part of the string
-    let middle = false;
+    let mut middle = false;
     for (i, ch) in input.chars().enumerate() {
         if ch == ' ' {
             if ! middle {
                 first += 1;
             } 
         } else {
+            middle = true;
             last = i;
         }
     }
-    (&input[first..last]).to_string()
+    (&input[first..(last + 1)]).to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{}{}",input.to_string(), " world!".to_string())
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    if let Some(idx) = input.find("cars") {
+        format!("{}balloons{}", &input[..idx].to_string(),
+                &input[(idx + 4)..].to_string())
+    } else {
+        input.to_string()
+    }
 }
 
 #[cfg(test)]
